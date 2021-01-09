f7cb35975db37a52afc618f8c8014e626e19f2c6
dpma <daniel.machado@xpand-it.com>
2021-01-09 18:53:42 +0000

    import styles

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1> Hello world! </h1>
+      <p> This is a paragraph </p>
+    </div>
+
+  )
 }
