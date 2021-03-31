d5f46ea3ae8794a9bf0ec65c22309349dc4189ef
dpma <daniel.machado@xpand-it.com>
2021-03-31 17:58:00 +0100

    convertion logic

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,32 +1,96 @@
 import React from "react"
 
-export default function Home() {
+class Home extends React.Component {
 
-  const handleSubmit = () => {
-    console.log('submit!!!!!!');
+  constructor(props) {
+    super(props);
+    this.state = {
+      red: '',
+      green: '',
+      blue: '',
+      alfa: '',
+      result: '',
+    };
   }
 
-  return (
-    <html>
-      <head>
-        <title>rgba to hex</title>
-      </head>
-      < body className="is-preload" >
-        < header id="header" >
-          <h1>rgba to hex</h1>
-          <p>A simple webpage which converts rgba values into hexadecimal<br />
-				asdasd.</p>
-        </header >
-        <form id="signup-form" onSubmit={handleSubmit} >
-          <input type="number" placeholder="red" min="0" max="255" />
-          <input type="number" placeholder="green" min="0" max="255" />
-          <input type="email" name="email" id="email" placeholder="Email Address" />
-          <input type="submit" value="Convert" />
-        </form >
+  hexTable = (value) => {
+    if (value < 10) return value.toString();
+    switch (value) {
+      case 10:
+        return 'a';
+      case 11:
+        return 'b';
+      case 12:
+        return 'c';
+      case 13:
+        return 'd';
+      case 14:
+        return 'e';
+      case 15:
+        return 'f';
+      default:
+    }
+  }
 
-        {/*  Scripts  */}
-        <script src="assets/js/main.js"></script>
-      </body >
-    </html >
-  )
+  convertToHex = (color) => {
+    const first = Math.floor(color / 16);
+    const second = 16 * (color / 16 - first);
+    console.log('second', second);
+    return this.hexTable(first) + this.hexTable(second);
+  }
+
+  convertAlfa = (alfa) => {
+    const value = 255 * alfa / 100;
+    return this.convertToHex(value);
+  }
+
+  handleSubmit = (event) => {
+    const { red, green, blue, alfa } = this.state;
+    const hexValue = '#' +
+      this.convertToHex(red) +
+      this.convertToHex(green) +
+      this.convertToHex(blue) +
+      this.convertAlfa(alfa);
+
+    this.setState({ result: hexValue });
+    event.preventDefault();
+  }
+
+  handleChange = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({ [nam]: val });
+  }
+
+  render() {
+    const { result } = this.state;
+    return (
+      <html>
+        <head>
+          <title>rgba to hex</title>
+        </head>
+        < body className="is-preload">
+          < header id="header" >
+            <h1>rgba to hex</h1>
+            <p>A simple webpage which converts rgba values into hexadecimal<br />
+				asdasd.</p>
+          </header >
+          <form id="signup-form" onSubmit={this.handleSubmit} >
+            <input type="number" placeholder="red" name="red" min="0" max="255" onChange={this.handleChange} />
+            <input type="number" placeholder="green" name="green" min="0" max="255" onChange={this.handleChange} />
+            <input type="number" placeholder="blue" name="blue" min="0" max="255" onChange={this.handleChange} />
+            <input type="number" placeholder="alfa" name="alfa" min="0" max="100" onChange={this.handleChange} />
+            {/* <input type="email" name="email" id="email" placeholder="Email Address" /> */}
+            <input type="submit" value="Convert" />
+          </form >
+          <p> hex value {result}</p>
+
+          {/*  Scripts  */}
+          {/* <script src="assets/js/main.js"></script> */}
+        </body >
+      </html >
+    )
+  }
 }
+
+export default Home;
