d401e4577b41324705082db6da634a0ff58aeab6
dpma <daniel.machado@xpand-it.com>
2021-03-31 16:23:48 +0100

    react forms submit function

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,48 +1,32 @@
 import React from "react"
 
 export default function Home() {
+
+  const handleSubmit = () => {
+    console.log('submit!!!!!!');
+  }
+
   return (
     <html>
       <head>
-        <title>Eventually by HTML5 UP</title>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-        {/* <link rel="stylesheet" href="assets/css/main.css" />  */}
+        <title>rgba to hex</title>
       </head>
-      <body class="is-preload">
-
-        {/* Header  */}
-        <header id="header">
+      < body className="is-preload" >
+        < header id="header" >
           <h1>rgba to hex</h1>
-          <p>A simple template for telling the world when you'll launch<br />
-				your next big thing. Brought to you by <a href="http://html5up.net">HTML5 UP</a>.</p>
-        </header>
-
-        {/* Signup Form */}
-        <form id="signup-form" method="post" action="#">
+          <p>A simple webpage which converts rgba values into hexadecimal<br />
+				asdasd.</p>
+        </header >
+        <form id="signup-form" onSubmit={handleSubmit} >
           <input type="number" placeholder="red" min="0" max="255" />
           <input type="number" placeholder="green" min="0" max="255" />
           <input type="email" name="email" id="email" placeholder="Email Address" />
-          <input type="submit" value="Sign Up" />
-        </form>
-
-        {/*  Footer  */}
-        <footer id="footer">
-          <ul class="icons">
-            <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-            <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-            <li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
-            <li><a href="#" class="icon fa-envelope"><span class="label">Email</span></a></li>
-          </ul>
-          <ul class="copyright">
-            <li>&copy; Untitled.</li><li>Credits: <a href="http://html5up.net">HTML5 UP</a></li>
-          </ul>
-        </footer>
+          <input type="submit" value="Convert" />
+        </form >
 
         {/*  Scripts  */}
         <script src="assets/js/main.js"></script>
-      </body>
-    </html>
-
+      </body >
+    </html >
   )
 }
